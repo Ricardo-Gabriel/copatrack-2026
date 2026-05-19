13fd67d5af98cb09a12d04151804e14a26cbd525
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2026-05-19 20:37:42 -0300

    Add GitHub Action for automated deployment and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/NOME-DO-REPOSITORIO/', // Descomente e altere se for subir para um subcaminho no GitHub Pages
 })
